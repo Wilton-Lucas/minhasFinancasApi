8bd48e68232f29e777cb4898c4e8316116dfde93
Wilton-Lucas <willstd00@gmail.com>
2021-06-08 17:19:51 -0300

    finalização classes modelo

--- src/main/java/com/app/financas/model/Categoria.java
+++ src/main/java/com/app/financas/model/Categoria.java
@@ -1,9 +1,23 @@
 package com.app.financas.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.app.financas.enums.TipoEnum;
 
-public class Categoria {
 
+
+@Entity
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private TipoEnum tipo;
 	private String descricao;
