4922e82a93fef625a46f1a556a209c39ae82b0b7
Wilton-Lucas <willstd00@gmail.com>
2021-06-30 19:36:14 -0300

    java version

--- src/main/java/com/app/financas/resources/exceptions/StandardError.java
+++ src/main/java/com/app/financas/resources/exceptions/StandardError.java
@@ -1,6 +1,8 @@
 package com.app.financas.resources.exceptions;
 
-public class StandardError {
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
