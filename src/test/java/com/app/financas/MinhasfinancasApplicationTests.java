50eaca09e9c3bf3a087b0c3cee1b416606186d3c
Wilton-Lucas <willstd00@gmail.com>
2021-07-19 15:41:43 -0300

    teste

--- src/test/java/com/app/financas/MinhasfinancasApplicationTests.java
+++ src/test/java/com/app/financas/MinhasfinancasApplicationTests.java
@@ -1,13 +1,26 @@
 package com.app.financas;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MinhasfinancasApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	void testeControllerGET(){
+		mockMvc.perform(get("http://localhost:8080/1")).andExpect(status().ok());
 	}
 
 }
